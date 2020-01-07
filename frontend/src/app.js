d01a94a7b17732987f01448d3663ff71e1f2f34e
JenniKate Wallace <jennikate@gmail.com>
2020-01-07 15:44:53 +0000

    setup

---
+++ frontend/src/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+const App = () => {
+
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Hi</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
