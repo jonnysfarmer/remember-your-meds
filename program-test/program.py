f2ddd7755dde95936bec344986c71a017ddb7a1d
jonnysfarmer <john.farmer@latimerross.com>
2020-01-08 15:08:24 +0000

    sorted out token, got data

--- program-test/program.py
+++ program-test/program.py
@@ -1,4 +1,6 @@
 import requests
 
-resp = requests.get('http://127.0.0.1:4000/api/medicines/')
-print(resp.json())
+resp = requests.get('http://127.0.0.1:4000/api/reminders')
+resp = resp.json()
+activereminders = list(filter(lambda ele: ele["active"], resp))
+print(activereminders)
