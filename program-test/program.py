84554f69d0643bc177ed637532bee006c4e8813a
jonnysfarmer <john.farmer@latimerross.com>
2020-01-08 16:24:40 +0000

    update

--- program-test/program.py
+++ program-test/program.py
@@ -3,4 +3,10 @@ import requests
 resp = requests.get('http://127.0.0.1:4000/api/reminders')
 resp = resp.json()
 activereminders = list(filter(lambda ele: ele["active"], resp))
-print(activereminders)
+# print(activereminders)
+
+
+test = list(map(lambda ele: {'name': ele['user']['username'], 'email': ele['user']['email'], 'medicine': ele['prescription']['medicine']['name']},  activereminders))
+
+for i in test:
+    print (i['name'])
