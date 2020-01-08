2fbc97975b1419bdd5e3e638291f499ce5bb91c4
JenniKate Wallace <jennikate@gmail.com>
2020-01-08 16:12:01 +0000

    reminder emails working

--- prescriptions/management/commands/sendemail.py
+++ prescriptions/management/commands/sendemail.py
@@ -1,23 +1,43 @@
 from django.core.management.base import BaseCommand
 import yagmail
+import requests
+
+
+
 
 class Command(BaseCommand):
     help = 'Sends user a reminder email'
 
-    sender_email = 'remember.your.meds.project@gmail.com'
-    receiver_email = 'jennikate@gmail.com'
-    subject = "Check cronjobs out"
+    def handle(self, *args, **kwargs):
 
-    # try: 
-    yag = yagmail.SMTP(user=sender_email)
+        resp = requests.get('http://localhost:4000/api/reminders')
+        resp = resp.json()
+        activereminders = list(filter(lambda ele: ele['active'], resp))
 
-    contents = [
-    "This is A NEW TEST again",
-    "As you can see, we can send a list of strings,",
-    "being this our third one",
-    ]
+        email_list = list(map(lambda ele: {'name': ele['user']['username'], 'email': ele['user']['email'], 'medicine': ele['prescription']['medicine']['name'], 'reminder_type': ele['reminder_type']},  activereminders))
 
-    yag.send(receiver_email, subject, contents)
+        print(email_list)
 
-    # except Exception as e:
-    # print(f'Something went wrong!e{e}')
+        for i in email_list:
+            email = i['email']
+            medicine = i['medicine']
+            user = i['name']
+            reminder_type = i['reminder_type']
+            
+            sender_email = 'remember.your.meds.project@gmail.com'
+            receiver_email = email
+            subject = (f'REMINDER! {reminder_type} {medicine}')
+
+            try: 
+                yag = yagmail.SMTP(user=sender_email)
+
+                contents = [
+                f"Hi {user}",
+                f"It's time to {reminder_type} your {medicine}",
+                "Cheers",
+                ]
+
+                yag.send(receiver_email, subject, contents)
+
+            except Exception as e:
+                self.stdout.write(f'There was an error!e{e}')
--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -4,7 +4,7 @@ from .views import MedicineListView, MedicineSpecificView, ReminderListView, Rem
 urlpatterns = [
     path('medicines/', MedicineListView.as_view()),
     path('medicines/<int:pk>/', MedicineSpecificView.as_view()),
-    path('reminders', ReminderListView.as_view()),
+    path('reminders/', ReminderListView.as_view()),
     path('reminders/<int:pk>/', ReminderSpecificView.as_view()),
     path('reminders/user/', ReminderUserView.as_view()),
     path('prescriptions/', PrescriptionListView.as_view()),
