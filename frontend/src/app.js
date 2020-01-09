9d4640fb3f93ea93ff60dc3c45d2f86b7d7b32ad
JenniKate Wallace <jennikate@gmail.com>
2020-01-09 13:41:15 +0000

    nav styling

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,18 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import ResponsiveDrawer from './components/Navbar'
 import Home from './components/Home'
 import Register from './components/Register'
 
 const App = () => {
 
-
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <ResponsiveDrawer />
       <Switch>
         <Route exact path="/1" component={Home} />
-        <Route exact path="/" component={Register} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   )
