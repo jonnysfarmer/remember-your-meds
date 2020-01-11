6475ec83774f4574e5fa9963beae517fe73d6914
JenniKate Wallace <jennikate@gmail.com>
2020-01-11 17:05:59 +0000

    calculating dates

--- frontend/src/components/Forms/EditOrderReminder.js
+++ frontend/src/components/Forms/EditOrderReminder.js
@@ -5,6 +5,8 @@ import { makeStyles, withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Switch from '@material-ui/core/Switch'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 import Box from '@material-ui/core/Box'
 import { red, green } from '@material-ui/core/colors'
@@ -56,8 +58,9 @@ const EditOrderReminder = (props) => {
 
   const classes = useStyles()
 
-  const [propsData, setPropsData] = useState({})
-  const [data, setData] = useState()
+  const [editing, setEditing] = useState() //determines whether data collection fields are visible or not
+  const [doses, setDoses] = useState() //only used in this component for calculating dates
+  const [data, setData] = useState() //data to save to reminders
   const [errors, setErrors] = useState()
 
   //===== INITIATE DATA FROM PROPS
@@ -66,17 +69,20 @@ const EditOrderReminder = (props) => {
     setData(props.props.propsData)
   }
 
-
-  //===== STORE FORM FIELD VALUES
-  const handleChange = (e) => {
-    if (e.target.name === 'active') {
-      const remState = (data.active = !data.active)
-      setData({ ...data, [e.target.name]: remState })
-
-      if (remState === true) {
-        calcReminderDue()
-      }
+  //===== STORE ACTIVE STATE
+  const handleActive = (e) => {
+    const remState = (data.active = !data.active)
+    setData({ ...data, ['active']: remState })
+    if (remState === true) {
+      setEditing(true)
+    } else {
+      setEditing(false)
     }
+  }
+
+  //===== STORE DOSE VALUE     
+  const handleChange = (e) => {
+    setDoses(e.target.value)
     setErrors({})
   }
 
@@ -85,12 +91,27 @@ const EditOrderReminder = (props) => {
     //----- start with today's date
     const startdate = moment() //'now'
     //----- add to it the number of doses user has remaining (due_date)
-
+    const dueDate = moment(startdate, "DD-MM-YYYY").add(doses, 'days').format('DD/MM/YYYY')
     //----- subtract from that 7 days as we will notify them 7 days in advance (reminder_date)
-    const t = moment(startdate, "DD-MM-YYYY").subtract(7, 'days').format('DD/MM/YYYY')
+    const reminderDate = moment(dueDate, "DD-MM-YYYY").subtract(7, 'days').format('DD/MM/YYYY')
 
     //----- handle if reminder_date is less than today
-    console.log(t)
+    console.log(dueDate, reminderDate)
+    //---- set data in our object
+    setData({
+      ...data,
+      ['due_time']: dueDate,
+      ['reminder_time']: reminderDate,
+      ['reminder_type']: 'order prescription'
+    })
+  }
+
+  //==== SUBMIT DATA
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //----calculate the dates
+    calcReminderDue()
+    console.log(data)
   }
 
 
@@ -101,9 +122,8 @@ const EditOrderReminder = (props) => {
 
 
   if (!data) return <div>loading</div>
-  console.log(data)
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
       <Typography component='h2' variant='h6'>
         Order a prescription
       </Typography>
@@ -116,7 +136,7 @@ const EditOrderReminder = (props) => {
               inputProps={{ 'aria-label': 'secondary checkbox' }}
               name='active'
               value={data.active}
-              onChange={e => handleChange(e)}
+              onChange={e => handleActive(e)}
             />
           </Grid>
           <Grid item>
@@ -127,6 +147,32 @@ const EditOrderReminder = (props) => {
           </Grid>
         </Grid>
       </Typography>
+
+      {editing === true && <>
+        <TextField
+          id='days_doses_remaining'
+          label='How many days doses do you have left?'
+          name='days_doses_remaining'
+          type='number'
+          required
+          helperText={'not including today\'s'}
+          variant='outlined'
+          fullWidth
+          margin='normal'
+          onChange={(e) => handleChange(e)}
+        />
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          color='primary'
+          className={classes.submit}
+        >
+          Add prescription
+        </Button>
+      </>
+      }
+
 
     </form>
 
