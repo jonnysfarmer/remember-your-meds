0570a4e55d5ee06d85d8fcd39e995a998310ae4b
jonnysfarmer <john.farmer@latimerross.com>
2020-01-16 12:01:00 +0000

    ready to deploy

--- prescriptions/management/commands/sendsms.py
+++ prescriptions/management/commands/sendsms.py
@@ -1,29 +1,32 @@
-from django.core.management.base import BaseCommand
-import requests
-from twilio.rest import Client
-from django.conf import settings
+# from django.core.management.base import BaseCommand
+# import requests
+# from twilio.rest import Client
+# from django.conf import settings
 
-class Command(BaseCommand):
-    help = 'Sends user a reminder sms'
 
-    def handle(self, *args, **kwargs):
+# THIS SENDS AN SMS - NOT FOR BUILD
 
-        resp = requests.get('http://localhost:4000/api/reminders/notifications')
-        resp = resp.json()
-        activereminders = list(filter(lambda ele: ele['active'], resp))
+# class Command(BaseCommand):
+#     help = 'Sends user a reminder sms'
 
-        sms_list = list(map(lambda ele: {'name': ele['user']['username'], 'mobile': '+44' + ele['user']['mobile'], 'medicine': ele['prescription']['medicine']['name'], 'reminder_type': ele['reminder_type']},  activereminders))
+#     def handle(self, *args, **kwargs):
 
-        print(sms_list)
+#         resp = requests.get('http://localhost:4000/api/reminders/notifications')
+#         resp = resp.json()
+#         activereminders = list(filter(lambda ele: ele['active'], resp))
 
-        for i in sms_list:
-            mobile = i['mobile']
-            medicine = i['medicine']
-            user = i['name']
-            reminder_type = i['reminder_type']
+#         sms_list = list(map(lambda ele: {'name': ele['user']['username'], 'mobile': '+44' + ele['user']['mobile'], 'medicine': ele['prescription']['medicine']['name'], 'reminder_type': ele['reminder_type']},  activereminders))
 
-            message_to_broadcast = (f'Hi {user}. It\'s time to {reminder_type}: {medicine}')
-            client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-            client.messages.create(to=mobile,
-                from_=settings.TWILIO_NUMBER,
-                body=message_to_broadcast)
+#         print(sms_list)
+
+#         for i in sms_list:
+#             mobile = i['mobile']
+#             medicine = i['medicine']
+#             user = i['name']
+#             reminder_type = i['reminder_type']
+
+#             message_to_broadcast = (f'Hi {user}. It\'s time to {reminder_type}: {medicine}')
+#             client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+#             client.messages.create(to=mobile,
+#                 from_=settings.TWILIO_NUMBER,
+#                 body=message_to_broadcast)
