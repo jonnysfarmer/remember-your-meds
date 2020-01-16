300e5f5618041871b3a09ae4c91a558e663ee9b4
jonnysfarmer <john.farmer@latimerross.com>
2020-01-16 12:04:00 +0000

    deploy

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
